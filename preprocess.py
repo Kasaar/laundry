493e43a12f2d909813c91c2760fd593ed02b5ef9
zelle105 <zelle105@umn.edu>
2024-11-21 12:51:31 -0600

    Skeleton code for converting timestamps to UNIX

--- preprocess.py
+++ preprocess.py
@@ -1,9 +1,28 @@
 import kagglehub
+import pandas as pd
+from datetime import datetime
 
 # Download latest version of the dataset
 path = kagglehub.dataset_download("ealtman2019/ibm-transactions-for-anti-money-laundering-aml")
+file_path = f"{path}/HI-Small_Trans.csv"
+df = pd.read_csv(file_path)
+
+print("Successfully read file")
 
 # Convert timestamps to UNIX format
+if 'Timestamp' in df.columns:
+    # Define the format the timestamp strings are in
+    date_format = "%Y/%m/%d %H:%M"
+
+    # Convert each timestamp
+    for ele in df['Timestamp']:
+        # Convert to datetime object
+        date_time = datetime.strptime(ele, date_format)
+
+        # Convert to UNIX timestamp
+        unix_timestamp = int(date_time.timestamp())
+
+        print(unix_timestamp)
 
 # Standardize sending bank designation
 
@@ -12,5 +31,3 @@ path = kagglehub.dataset_download("ealtman2019/ibm-transactions-for-anti-money-l
 # Standardize receiving bank designation
 
 #  Standardize receiving account number
-
-print("Path to dataset files:", path)
